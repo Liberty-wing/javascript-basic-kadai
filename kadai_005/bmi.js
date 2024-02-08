14d137baed03ef255335329692b5a59e55389f87
Liberty-wing <157755624+Liberty-wing@users.noreply.github.com>
2024-02-08 21:16:26 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,13 +1,11 @@
-let weight;
+//体重の値宣言・代入
+let weight = 68;
 
-weight = 68;
+//身長の値宣言・代入
+let height = 1.7;
 
-let height;
+//BMIの値宣言・代入
+let bmi = (weight/(height*height));
 
-height = 1.7;
-
-let bmi;
-
-bmi = (weight/(height*height));
-
+//BMI コンソールへの出力
 console.log(bmi);
