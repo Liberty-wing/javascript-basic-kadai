1eb7b69b5dd00d8e33daa6b812d8345857fc5838
Liberty-wing <157755624+Liberty-wing@users.noreply.github.com>
2024-02-12 21:46:21 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,6 +1,6 @@
-const date = new Date(2023,8,22);
+const date = new Date();
   date.getFullYear();
   date.getMonth();
   date.getDate();
 
-  console.log(date.getFullYear()+'年'+date.getMonth()+'月'+date.getDate()+'日');
+  console.log(date.getFullYear()+'年'+(date.getMonth()+1)+'月'+date.getDate()+'日');
